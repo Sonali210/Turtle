90d2774277475d6ccb949584a4f7f49289b5d13d
Sonali <sonali21031@gmail.com>
2020-06-19 16:54:10 +0530

    Add files via upload

---
+++ Turtle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import turtle
+turtle.speed(0)
+turtle.bgcolor("Green")
+turtle.shape("arrow")
+turtle.pensize("2")
+n=150
+dist=2
+for _ in range(n):
+  if _ < 100:
+    turtle.color("Brown")
+    dist += 2
+  else:
+    turtle.color("Yellow")
+    turtle.stamp()
+    dist += 2
+  turtle.fd(dist)
+  turtle.left(137.5)
+  
+
+
+# In[ ]:
+
+
+
+
